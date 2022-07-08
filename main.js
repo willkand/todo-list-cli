5e327da2c3d0c3517cb0e31b8109effdb7766c6a
Andre K <andrek@Andres-MacBook-Pro.local>
2022-07-08 14:00:54 -0400

    2n commit

--- main.js
+++ main.js
@@ -38,3 +38,27 @@ while (selection !== 3){
     if (selection !== 1 && selection !== 2 && selection !== 3){
             console.log("________________________________________________");
     }
+
+    if (selection === 1){
+        console.log("You chose to crete a task for your to-do list. ");
+        console.log("What task would you like to create? ");
+        add = prompt("> ");
+        todoList.push(add);
+        console.log("________________________________________________");
+        console.log("You have " + Number(todoList.length) + " tasks in your to-do list. ");
+
+        for(let i = 0; i < todoList.length; i++){
+                completed[todoList.length -1] = false;
+
+                if (completed[todoList.length - 1] === false){
+                        status[i] = "[Incomplete] ";
+                }
+                else if (complete[todoList.length - 1] === true){
+                        status[i] = "[Complete] ";
+                }
+
+        console.log(Number(i + 1) + "." + status[i] + todoList[i]);
+        // console.log("________________________________________________");
+
+        }
+    }
